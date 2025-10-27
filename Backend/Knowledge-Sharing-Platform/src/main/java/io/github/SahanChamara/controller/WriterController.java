8c3300d908d9697901b1502cdfcc1653c4563591
Sahan Chamara <sahanchamara456@gmail.com>
2025-10-28 00:06:54 +0530

    add WriterController with GraphQL mutations and queries for writers

--- Backend/Knowledge-Sharing-Platform/src/main/java/io/github/SahanChamara/controller/WriterController.java
+++ Backend/Knowledge-Sharing-Platform/src/main/java/io/github/SahanChamara/controller/WriterController.java
@@ -1,4 +1,36 @@
 package io.github.SahanChamara.controller;
 
+import io.github.SahanChamara.dto.Writer;
+import io.github.SahanChamara.service.WriterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class WriterController {
+
+    private final WriterService writerService;
+
+    @MutationMapping()
+    public Writer addWriter(@Argument("input") Writer writerInput){
+        return writerService.addWriter(new Writer(null,writerInput.getName(), writerInput.getBio()));
+    }
+
+    @QueryMapping()
+    public List<Writer> getWriters(){
+        return writerService.getAllWriters();
+    }
+
+    @QueryMapping()
+    public Writer getWriterById(Long id){
+        if(id != null){
+            return writerService.getWriterById(id);
+        }
+        return null;
+    }
 }
