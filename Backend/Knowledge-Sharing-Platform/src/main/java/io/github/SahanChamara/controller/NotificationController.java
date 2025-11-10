0285c019172fe8d9d29b9bfc279f023fb36bd4fc
SahanChamara <sahanchamara456@gmail.com>
2025-11-10 15:26:47 +0530

    add NotificationController to manage notifications, including retrieval, unread count, marking as read, and subscription for new notifications

---
+++ Backend/Knowledge-Sharing-Platform/src/main/java/io/github/SahanChamara/controller/NotificationController.java
@@ -0,0 +1,44 @@
+package io.github.SahanChamara.controller;
+
+import io.github.SahanChamara.dto.Notification;
+import io.github.SahanChamara.publisher.NotificationPublisher;
+import io.github.SahanChamara.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationController {
+    private final NotificationService notificationService;
+    private final NotificationPublisher notificationPublisher;
+
+    @QueryMapping
+    public List<Notification> notifications(@Argument Long recipientId){
+        return notificationService.getNotificationsForRecipient(recipientId);
+    }
+
+    @QueryMapping
+    public Long unreadNotificationCount(@Argument Long recipientId){
+        return notificationService.countUnread(recipientId);
+    }
+
+    @MutationMapping
+    public Boolean markNotificationRead(@Argument Long notificationId){
+        notificationService.markAsRead(notificationId);
+        return true;
+    }
+
+    @SubscriptionMapping
+    public Flux<Notification> notificationAdded(@Argument Long recipientId){
+        return notificationPublisher.getPublisher().filter(notification -> notification.getRecipientId().equals(recipientId));
+    }
+}
